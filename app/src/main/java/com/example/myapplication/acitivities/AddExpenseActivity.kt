487e564c5a696be50fccd055c50ac65d56952927
MD. Tanvir Hossain Saikat <tanvirhossain3@iut-dhaka.edu>
2023-05-27 14:42:24 +0600

    added add expense activity

--- app/src/main/java/com/example/myapplication/acitivities/AddExpenseActivity.kt
+++ app/src/main/java/com/example/myapplication/acitivities/AddExpenseActivity.kt
@@ -1,97 +1,72 @@
 package com.example.myapplication.acitivities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.EditText
-import androidx.core.widget.addTextChangedListener
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
-import com.example.myapplication.Database
-import com.example.myapplication.models.CategoryModel
+import android.widget.*
 import com.example.myapplication.R
-import com.example.myapplication.adapters.CategoryAdapter
-import com.example.myapplication.models.ExpenseModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class AddExpenseActivity : AppCompatActivity() {
-
-    lateinit var categoryRV: RecyclerView
-    lateinit var categoryRVAdapter: CategoryAdapter
-    lateinit var categoryList: ArrayList<CategoryModel>
+    private lateinit var dropdownRow: LinearLayout
+    private lateinit var dropdown1: Spinner
+    private lateinit var dropdown2: Spinner
+    private lateinit var saveButton: Button
+    private lateinit var buttonRow1: LinearLayout
+    private lateinit var button7: Button
+    private lateinit var button8: Button
+    private lateinit var button9: Button
+    private lateinit var buttonRow2: LinearLayout
+    private lateinit var button4: Button
+    private lateinit var button5: Button
+    private lateinit var button6: Button
+    private lateinit var buttonRow3: LinearLayout
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var buttonRow4: LinearLayout
+    private lateinit var buttonDot: Button
+    private lateinit var button0: Button
+    private lateinit var buttonBack: Button
+    private lateinit var date: TextView
+    private lateinit var note: TextView
+    private lateinit var title: TextView
+    private lateinit var cancelButton: TextView
+    private lateinit var dollarIcon: ImageView
+    private lateinit var amount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_expense)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-        categoryRV = findViewById(R.id.categoryGrid)
 
-        categoryList = ArrayList()
+        // Assign variables to layout items
+        dropdownRow = findViewById(R.id.dropdownRow)
+        dropdown1 = findViewById(R.id.dropdown1)
+        dropdown2 = findViewById(R.id.dropdown2)
+        saveButton = findViewById(R.id.saveButton)
+        buttonRow1 = findViewById(R.id.buttonRow1)
+        button7 = findViewById(R.id.button7)
+        button8 = findViewById(R.id.button8)
+        button9 = findViewById(R.id.button9)
+        buttonRow2 = findViewById(R.id.buttonRow2)
+        button4 = findViewById(R.id.button4)
+        button5 = findViewById(R.id.button5)
+        button6 = findViewById(R.id.button6)
+        buttonRow3 = findViewById(R.id.buttonRow3)
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
+        buttonRow4 = findViewById(R.id.buttonRow4)
+        buttonDot = findViewById(R.id.buttonDot)
+        button0 = findViewById(R.id.button0)
+        buttonBack = findViewById(R.id.buttonBack)
+        date = findViewById(R.id.date)
+        note = findViewById(R.id.note)
+        title = findViewById(R.id.title)
+        cancelButton = findViewById(R.id.cancel_button)
+        dollarIcon = findViewById(R.id.dollarIcon)
+        amount = findViewById(R.id.amount)
 
-        val layoutManager = GridLayoutManager(this, 4)
-
-        categoryRV.layoutManager = layoutManager
-
-        categoryRVAdapter = CategoryAdapter(categoryList, this)
-
-        categoryRV.adapter = categoryRVAdapter
-
-
-        populateCategories()
-
-        categoryRVAdapter.notifyDataSetChanged()
-
-        val saveBtn = findViewById<Button>(R.id.saveBtn)
-
-        saveBtn.setOnClickListener {
-            val amt = findViewById<EditText>(R.id.amount)
-            val title = findViewById<EditText>(R.id.expTitle)
-            val note = findViewById<EditText>(R.id.note)
-            amt.addTextChangedListener{
-                if(it!!.isNotEmpty())
-                    amt.error = null
-            }
-            if(amt.text.toString().toDoubleOrNull()==null) amt.error = "Please enter an amount"
-            else {
-                val expense = ExpenseModel(0, R.drawable.burger, title.text.toString() , note.text.toString(), amt.text.toString().toDouble())
-                insert(expense)
-            }
-            val intent = Intent(this, HomePage::class.java)
-            startActivity(intent)
-        }
+        // Rest of your code...
     }
 
-    private fun populateCategories()
-    {
-        categoryList.add(CategoryModel("Snacks", R.drawable.burger))
-        categoryList.add(CategoryModel("Transport", R.drawable.ic_transportation))
-        categoryList.add(CategoryModel("Meals", R.drawable.ic_restaurant))
-        categoryList.add(CategoryModel("Study", R.drawable.ic_study))
-        categoryList.add(CategoryModel("Tools", R.drawable.ic_tools))
-        categoryList.add(CategoryModel("Subscription", R.drawable.ic_subscription))
-        categoryList.add(CategoryModel("Medicine", R.drawable.ic_medicine))
-        categoryList.add(CategoryModel("Groceries", R.drawable.ic_groceries))
-        categoryList.add(CategoryModel("Clothing", R.drawable.ic_clothing))
-        categoryList.add(CategoryModel("Pet", R.drawable.ic_pet))
-        categoryList.add(CategoryModel("Misc.", R.drawable.soda))
-        categoryList.add(CategoryModel("Lend", R.drawable.ic_lend))
-    }
-
-    private fun insert(expense : ExpenseModel)
-    {
-        val db = Room.databaseBuilder(this,
-            Database::class.java,
-            "Expenses").build()
-
-        GlobalScope.launch {
-            db.expenseDao().insertAll(expense)
-        }
-    }
+    // Rest of the AddExpenseActivity class...
 }
